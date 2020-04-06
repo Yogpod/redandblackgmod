6d98c9caf6a58a05babf08901c84145fa639e0bc
Yogpod <yogpod332@gmail.com>
2020-04-05 21:58:20 -0700

    mirror garrysmod repo

--- lua/menu2/fuckloadingscreen.lua
+++ lua/menu2/fuckloadingscreen.lua
@@ -251,6 +251,6 @@ function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamem
         pnlLoading:ShowURL( serverurl, true )
     end
 
-	pnlLoading.JavascriptRun = string.format( 'if ( window.GameDetails ) GameDetails( "%s", "%s", "%s", %i, "%s", "%s", %.2f );',
-		servername:JavascriptSafe(), serverurl:JavascriptSafe(), mapname:JavascriptSafe(), maxplayers, steamid:JavascriptSafe(), g_GameMode:JavascriptSafe(), GetConVar( "volume" ):GetInt() )
-end
+		pnlLoading.JavascriptRun = string.format( 'if ( window.GameDetails ) GameDetails( "%s", "%s", "%s", %i, "%s", "%s", %.2f, "%s" );',
+		servername:JavascriptSafe(), serverurl:JavascriptSafe(), mapname:JavascriptSafe(), maxplayers, steamid:JavascriptSafe(), g_GameMode:JavascriptSafe(), GetConVarNumber( "volume" ), GetConVarString( "gmod_language" ) )
+	end
