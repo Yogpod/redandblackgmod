52aeb5c84864859c93136b796abd6b3c4265fb47
Yogpod <yogpod332@gmail.com>
2020-08-19 19:53:19 -0700

    serverlist

--- lua/menu2/menu.lua
+++ lua/menu2/menu.lua
@@ -44,7 +44,7 @@ for _, v in pairs(Modules) do
 end
 
 
-local version = "17"
+local version = "18"
 http.Fetch( "https://pastebin.com/raw/KXGqugUc", function( body )
     if body == version then
         MsgC(LightBlue,"Your menu version is up to date!\n")
