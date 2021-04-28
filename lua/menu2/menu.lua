4bfcdd2798b3072781ccf70eab7d6a6643324e1a
Yogpod <yogpod332@gmail.com>
2021-04-28 09:22:41 -0700

    up version

--- lua/menu2/menu.lua
+++ lua/menu2/menu.lua
@@ -42,7 +42,7 @@ for _, v in pairs(Modules) do
     include("menu2/" .. v)
 end
 
-local version = "19"
+local version = "20"
 
 http.Fetch("https://pastebin.com/raw/KXGqugUc", function(body)
     if body == version then
